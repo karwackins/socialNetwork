980a1ad1f73082ebc66c562834a01499d34d2d7b
karwackid <damian.karwacki@slaskie.pl>
2020-01-05 08:53:57 +0100

    konfigurowanie seedera do zakladania losowych uzytkownikow

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,46 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $faker = Faker::create('pl_PL');
+
+        //------------------------------------------------
+        $number_of_users = 20;
+        $pass = 'pass';
+        //------------------------------------------------
+
+        for($i=1; $i <= $number_of_users; $i++)
+        {
+            if($i === 1)
+            {
+                DB::table('users')->insert([
+                    'name' => 'Damian Karwacki',
+                    'email' => 'damian@damian.pl',
+                    'sex' => 'm',
+                    'password' => Hash::make('damian'),
+                ]);
+            } else
+            {
+                $sex = $faker->randomElement(['m', 'f']);
+
+                switch ($sex){
+                    case 'm':
+                        $name = $faker->firstNameMale.' '.$faker->lastName;
+                        break;
+
+                    case 'f':
+                        $name = $faker->firstNameFemale.' '.$faker->lastName;
+                }
+                DB::table('users')->insert([
+                    'name' => $name,
+                    'email' => str_replace('-','',str_slug($name).'@'.$faker->safeEmailDomain),
+                    'sex' => $sex,
+                    'password' => Hash::make($pass),
+                ]);
+            }
+
+
+
+        }
+
     }
 }
