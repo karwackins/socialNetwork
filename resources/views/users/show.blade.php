9e4d224f5fc1baae53d30409878c3256461ecf75
karwackid <damian.karwacki@slaskie.pl>
2020-01-17 12:18:30 +0100

    fix dodawanie postow przez usera

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,7 +9,7 @@
                     <div class="card-body">
                         <form action="{{url('/posts')}}" method="POST">
                             {{ csrf_field() }}
-                            <textarea class="form-control{{ $errors->has('post_content') ? ' is-invalid' : '' }}" name="post_content" id="post_content" cols="30" rows="5" placeholder="Co tam słychać?">{{ old() }}</textarea>
+                            <textarea class="form-control{{ $errors->has('post_content') ? ' is-invalid' : '' }}" name="post_content" id="post_content" cols="30" rows="5" placeholder="Co tam słychać?">{{ old('post_content') }}</textarea>
                             @if ($errors->has('post_content'))
                                 <span class="invalid-feedback" role="alert">
                                                 <strong>{{ $errors->first('post_content') }}</strong>
